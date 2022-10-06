2d26b037c0e933234bd2b4d0faa9d1ef43349cea
expendable1656 <97333284+expendable1656@users.noreply.github.com>
2022-10-06 16:44:06 +0800

    A Roblox module for function and signal handling
    Go use it I guess

---
+++ ReExec.lua
@@ -0,0 +1,47 @@
+local ReExec = {}
+local Existing = {}
+ReExec.__index = ReExec
+ReExec.__newindex = newcclosure(function(self, key, value)
+	print(key)
+	if self ~= ReExec and key == "AddIndex" then	
+		self:Add(value)
+	end
+end)
+
+function ReExec.new(String)
+	if Existing[String] then
+		return Existing[String]
+	end
+	local Thing = {
+		Connections = {},
+		Threads = {}
+	}
+    Existing[String] = Thing
+	return setmetatable(Thing, ReExec)
+end
+
+function ReExec:Add(Thing)
+	local Type = typeof(Thing)
+	if Type == "RBXScriptConnection" then
+		table.insert(self.Connections, Thing)
+	elseif Type == "function" then
+		local Thread
+		local Wrapped = coroutine.wrap(newcclosure(function()
+			Thread = coroutine.running()
+			Thing()
+		end))
+		task.spawn(Wrapped)
+		table.insert(self.Threads, Thread)
+	end
+end
+
+function ReExec:Cleanup()
+	for i,v in pairs(self.Connections) do
+		v:Disconnect()
+	end
+	for i,v in pairs(self.Threads) do
+		pcall(task.cancel, v)
+	end
+end
+
+getgenv().ReExec = ReExec
